73560b21c61a1d24dc789a47b7d366b357177602
Jack Belnik <jw.belnik@gmail.com>
2023-08-30 17:12:37 +0300

    feat: task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.prev = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.prev = Math.ceil((this.min + this.max) / 2);
+        return this.prev;
     }
 
     lower() {
-
+        this.max = this.prev;
     }
 
     greater() {
-
+        this.min = this.prev;
     }
 }
 
